23a7540a27d7fba9946c3e1c6ceb7aea50d84a7e
dietervdbn <dieter.vanderbemden@gmail.com>
2021-07-08 14:55:45 +0200

    Add project files.

---
+++ wpfBasic/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+
+namespace wpfBasic
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+        }
+    }
+}
